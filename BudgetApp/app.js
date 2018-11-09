c274c521c378f1995dbc298a04a327759ae2e88a
codingBlock <niranjans3142@gmail.com>
2018-11-09 20:40:21 +0530

    js updated

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,9 +1,11 @@
 var budgetController = (function () {
-    var incomeProto = function(description,value){
+    var incomeProto = function(id,description,value){
+        this.id=id;
         this.description=description;
         this.value=value;
     };
-    var expenseProto = function(description,value,percentage){
+    var expenseProto = function(id,description,value,percentage){
+        this.id=id,
         this.description=description;
         this.value=value;
         this.percentage=percentage;
@@ -23,16 +25,42 @@ var uiController = (function () {
         addDescription: '.add__description',
         addValue: '.add__value',
         addButton: '.add__btn',
+        addHTMLIncome : '.income__list',
+        addHTMLExpense : '.expenses__list'
     };
 
+    var addHTMLToPage = function(obj,type){
+        var html,newHTML;
+        if(type === 'income'){
+            
+            html = '<div class="item clearfix" id="income-&id&"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            
+            newHTML = html.replace('%id%',obj.id);
+            newHTML = html.replace('%description%',obj.description);
+            newHTML = html.replace('%value%',obj.value);
+            console.log(newHTML)
+            document.querySelector(className.addHTMLIncome).insertAdjacentHTML('beforeend',newHTML);
+        }else{
+
+            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+            newHTML = html.replace('%id%',obj.id);
+            newHTML = html.replace('%description%',obj.description);
+            newHTML = html.replace('%value%',obj.value);
+            console.log(newHTML)
+            document.querySelector(className.addHTMLExpense).insertAdjacentHTML('beforeend',newHTML);
+        }
+    }
+    
     return{
-        className
+        className,
+        addHTMLToPage
     }
 
 })();
 
 var appController = (function (budgetCtrl, uiCtrl) {
-    var type, description, value;
+    var type, description, value, idIncome=1, idExpense=1;
     
     var eventCodeHandle = function () {
         type = document.querySelector(uiCtrl.className.addType).value;
@@ -41,10 +69,21 @@ var appController = (function (budgetCtrl, uiCtrl) {
         
         //add item to list
         if(type === 'income'){
-            budgetCtrl.incomeArray.push(new budgetCtrl.incomeProto(description,value))
-        }else{
-            budgetCtrl.expenseArray.push(new budgetCtrl.expenseProto(description,value))
+            budgetCtrl.incomeArray.push(new budgetCtrl.incomeProto(idIncome,description,value));
+            //add item to UI
+            uiCtrl.addHTMLToPage(budgetCtrl.incomeArray[idIncome-1],type);
+            idIncome++;
+        }else{  
+            budgetCtrl.expenseArray.push(new budgetCtrl.expenseProto(idExpense,description,value));
+            //add item to UI
+            uiCtrl.addHTMLToPage(budgetCtrl.expenseArray[idExpense-1],type);
+            idExpense++;
         }
+
+
+        //cleaning value from description value
+        document.querySelector(uiCtrl.className.addDescription).value="";
+        document.querySelector(uiCtrl.className.addValue).value="";
     }
 
     //handle event listner
@@ -56,7 +95,7 @@ var appController = (function (budgetCtrl, uiCtrl) {
         }
     })
     
-    //add item to UI
+    
 
 
 })(budgetController, uiController);
